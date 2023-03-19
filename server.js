479323c86839b5027a271dc6b8bd576d4594d147
Sangsaka Wira <sangsakawira@gmail.com>
2023-03-19 14:36:20 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const os = require('os');
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const upload = multer({
+  limits: { fileSize: 20 * 1024 * 1024 }, // 20 MB limit
+  storage: multer.diskStorage({
+    destination: (req, file, callback) => {
+      const storageFolder = './storage';
+      // Check if the storage folder exists, create it if it doesn't
+      if (!fs.existsSync(storageFolder)) {
+        fs.mkdirSync(storageFolder);
+      }
+      callback(null, storageFolder); // Save files to the 'storage' folder
+    },
+    filename: (req, file, callback) => {
+        const extension = path.extname(file.originalname);
+        const uniqueName = `${uuidv4()}${extension}`; // Use UUIDs for the filename
+        callback(null, uniqueName);
+    }
+  })
+});
+
+app.use('/storage', express.static(path.join(__dirname, 'storage')));
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send({ file: req.file });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running!')
+})
